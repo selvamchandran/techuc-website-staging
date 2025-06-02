2fc003b0dfa9386a723a2f1d029ba1cda16fee62
selvamchandran <selvam.v.chandran@gmail.com>
2025-06-02 20:18:33 +0530

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+// Scroll to top button
+  const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+      scrollToTopBtn.classList.remove('hidden');
+    } else {
+      scrollToTopBtn.classList.add('hidden');
+    }
+  });
+
+  scrollToTopBtn.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+
